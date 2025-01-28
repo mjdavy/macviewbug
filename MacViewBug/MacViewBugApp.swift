4f69763bf403b2796ecc208e0a1e47fecd94e223
Martin Davy <martin.davy@gmail.com>
2025-01-28 10:42:24 -0500

    Initial Commit

---
+++ MacViewBug/MacViewBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacViewBugApp.swift
+//  MacViewBug
+//
+//  Created by Martin Davy on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MacViewBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
